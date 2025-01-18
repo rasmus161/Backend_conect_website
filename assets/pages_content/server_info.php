833cd1820e19dd06d359f4ec94a09c21007d3fbf
rasmusp <porthinr@arcada.fi>
2025-01-18 15:28:36 +0200

    add cookies + display info abour server user

--- assets/pages_content/server_info.php
+++ assets/pages_content/server_info.php
@@ -1,5 +1,4 @@
 <?php
 echo " <p>sever name: ";
 echo $_SERVER['SERVER_NAME'] . "</p>";
-echo "<br>";
 echo " <P>server time zone: " . date_default_timezone_get() . "</p>";
--- assets/pages_content/user_info.php
+++ assets/pages_content/user_info.php
@@ -1,8 +1,23 @@
 <?php
-/*
-cockies
- -cockie on first visit
- - tiden när deta hände
-name 
-ip adress
-*/
+
+$cookie_name = "user";
+$cookie_value = "rase";
+$cookie_time = "first_visit_time";
+
+if (!isset($_COOKIE[$cookie_name])) {
+    setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
+    setcookie($cookie_time, date("Y-m-d H:i:s"), time() + (86400 * 30), "/");
+}
+?>
+<html>
+<?php
+
+if (!isset($_COOKIE[$cookie_name])) {
+    echo "Cookie named '" . $cookie_name . "' is not set!";
+} else {
+    echo "First visited : " . $_COOKIE[$cookie_time] . "<br>";
+    echo "<p>User name: " . $_COOKIE[$cookie_name] . "</p>";
+    echo "<p>User IP address: " . $_SERVER['REMOTE_ADDR'] . "</p>";
+}
+
+?>
