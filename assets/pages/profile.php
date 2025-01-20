4cb470902bba680b961eb0bbced1140545433653
rasmusp <porthinr@arcada.fi>
2025-01-20 13:43:37 +0200

    stype changes to profile

--- assets/pages/profile.php
+++ assets/pages/profile.php
@@ -22,13 +22,43 @@
 
     <?php include "../pages_content/header.php" ?>
     <main>
-        <div class="container">
+
+        <div class="container" id="profile-content">
+            <div id="left">
+                <h3>name here</h3>
+                <img src="../img/" alt="profile image">
+                <div id="socials-container">
+                    <img src="" alt="test1">
+                    <img src="" alt="test2">
+                    <img src="" alt="test3">
+
+                </div>
+            </div>
+            <div id="right">
+                <div id="start-chat-button">
+                    <button class="button-small">start chat</button>
+                </div>
+                <div id="bio">
+                    <div id="title">
+                        <h3>Bio</h3>
+                    </div>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum has been the industry's standard dummy text ever since the
+                        1500s, when an unknown printer took a galley of type and scrambled it to
+                        make a type specimen book
+                    </p>
+
+
+                </div>
+
+            </div>
 
         </div>
 
         <div class="container">
             <div class="container-inner" id="container-preferences">
-                <div class="half id=" preferences-favorite">
+                <div class="half" id="preferences-favorite">
                     <h3>Favorite activities</h3>
                     <form method="post">
                         <label><input type="checkbox" name="favorite_activities[]" value="Reading"> Reading</label><br>
@@ -40,7 +70,7 @@
                     </form>
 
                 </div>
-                <div class="half id=" preferences-less-preferred">
+                <div class="half " id=" preferences-less-preferred">
                     <h3>Less preferred activities</h3>
                     <form method="post">
                         <label><input type="checkbox" name="less_preferred_activities[]" value="Reading"> Reading</label><br>
