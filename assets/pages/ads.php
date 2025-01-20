82e8467507c97aa116479a35afa9d5e885c7af34
rasmusp <porthinr@arcada.fi>
2025-01-20 16:16:28 +0200

    more styling yay neads more work and changing

--- assets/pages/ads.php
+++ assets/pages/ads.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../main.css">
+    <link rel="stylesheet" href="../../assets/css/abs.css">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -15,10 +16,44 @@
 
 <body>
     <?php include "../pages_content/header.php" ?>
-    <h1>ads</h1>
+
     <main>
         <div class="container" id="filter">
-            
+            <div>
+                <h3>preferred</h3>
+                <?php include "../pages_content/preference_forms.php" ?>
+            </div>
+            <div>
+                <h3>less preferred</h3>
+                <?php include "../pages_content/preference_forms.php" ?>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="container-inner" id="profile-abs">
+                <div id="left">
+                    <h3>name here</h3>
+                    <img src="../img/" alt="profile picture">
+                </div>
+                <div id="right">
+                    <div id="bio-text">
+                        <!-- profile content text img name import!-->
+                        <p>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            Lorem Ipsum has been the industry's standard dummy text ever since the
+                            1500s, when an unknown printer took a galley of type and scrambled it to
+                            make a type specimen book
+                        </p>
+                    </div>
+                    <div id="preferred">
+                        <!-- top liked to do activities import !-->
+                        <p>import 1</p>
+                        <p>import 2</p>
+                        <p>import 3</p>
+                    </div>
+                </div>
+
+            </div>
         </div>
 
 
