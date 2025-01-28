f9d047e74d456d0dfa5f4866615d0cd95abf93b2
rasmusp <porthinr@arcada.fi>
2025-01-28 17:28:45 +0200

    fix uppg 6 yay

--- assets/pages_content/uppgift6/visit_count.php
+++ assets/pages_content/uppgift6/visit_count.php
@@ -8,30 +8,41 @@ $ip = $_SERVER['REMOTE_ADDR'];
 $time = date("Y-m-d H:i:s");
 
 // read fille content into a array
-$visits = file_exists($fileName) ? file(FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES) : [];
+$visits = file_exists($fileName) ? file($fileName, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES) : [];
 
 // check if it is the users first visit or not
+
 $isNewVisitor = true;
 foreach ($visits as $visit) {
     list($visitIp, $visitTime) = explode(" - ", $visit); // separate visit time and ip
     if ($visitIp == $ip) {
-        $isNewVisitor  = false;
+        $isNewVisitor = false;
         break;
     }
 }
 
-// add new visitors to the fille
+// unique visit's count
+
+$uniqueIps = [];
+foreach ($visits as $visit) {
+    $visitIp = explode(" - ", $visit)[0];
+    if (!in_array($visitIp, $uniqueIps)) {
+        $uniqueIps[] = $visitIp;
+    }
+}
+
+$uniqueVisitors = count($uniqueIps) + ($isNewVisitor ? 1 : 0);
+
+// add new visitors to the fille + unique visitors count
 
 if ($isNewVisitor) {
-    $myfile = fopen("visit_count.txt", "a") or die("unable to open file no5! ");
+    $myfile = fopen($fileName, "a") or die("unable to open file no1! ");
 
     // write to the fille 
     fwrite($myfile, "$ip - $time\n");
+    fwrite($myfile, "$uniqueVisitors unique visitors have visited your site\n");
+
 
     // close  the fille
     fclose($myfile);
 }
-
-// show amount of unique visitors
-$uniqueVisitors = count($visits) + ($isNewVisitor ? 1 : 0);
-echo "Unique visitor count: $uniqueVisitors";
