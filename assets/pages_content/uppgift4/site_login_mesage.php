944cb4e50f27cd33ff993335c24df56a29951f82
rasmusp <porthinr@arcada.fi>
2025-01-29 14:05:38 +0200

    we are back in buisnes fixed

--- assets/pages_content/uppgift4/site_login_mesage.php
+++ assets/pages_content/uppgift4/site_login_mesage.php
@@ -1,4 +1,11 @@
 <?php
+
+$_SESSION['name'] = "test_name";
+
+echo "whelcome " . $_SESSION['name'] . "!";
+
+
+
 if (isset($_COOKIE['first_visit_time'])) {
     echo "<h2>Welcome back! " . $_COOKIE['user'] ." You visited us for the first time in ". $_COOKIE['first_visit_time']."</h2>";
 } else {
