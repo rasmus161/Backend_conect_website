c7c48d98921d927595f437721a0e0a944bc8eb08
rasmusp <porthinr@arcada.fi>
2025-01-30 15:53:03 +0200

    ree its broken

--- assets/pages_content/uppgift7/display_coments.php
+++ assets/pages_content/uppgift7/display_coments.php
@@ -1,8 +1,6 @@
 <?php
-
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-
 $file = 'comments.json';
 
 if(file_exists($file)){
@@ -10,13 +8,8 @@ if(file_exists($file)){
 
     if ($comments){
         foreach ($comments as $comment){
-            
-            echo '<div class="comment">';
-            echo '<p><strong>' . htmlspecialchars($comment['chatName']) . '</strong> at ' . htmlspecialchars($comment['chatTime']) . '</p>';
-            echo '<p>' . htmlspecialchars($comment['comment']) . '</p>';
-            echo '</div>';
+            echo "". htmlspecialchars($comment['chatTime']) . htmlspecialchars($comment['chatName']). htmlspecialchars($comment['comment']) ;
+        
         }
-    }else {
-        echo '<p>No comments found.</p>';
     }
 }
