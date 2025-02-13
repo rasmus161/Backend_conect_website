ef14b0fb30c1051aed605868cd42be9786ddee81
rasmus161 <rasmus.porthin@gmai.com>
2025-02-13 11:19:06 +0200

    START OF PROJECT 2

---
+++ testing.php
@@ -0,0 +1,3 @@
+<?php
+
+// INSERT INTO `profiles` (`id`, `username`, `realname`, `passhash`, `zipcode`, `bio`, `salery`, `preference`, `email`, `likes`, `role`) VALUES (NULL, 'porhir', 'rasmus', 'superhemlis', '90990', 'hej i am cool', '3000', '2', 'porthinr@arcada.fi', '994', '4');
